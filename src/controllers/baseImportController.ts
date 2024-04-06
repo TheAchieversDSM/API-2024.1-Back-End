be48a119ba99e953f071f3f8b70e08fe2e87219e
= <=>
2024-04-06 18:24:10 -0300

    Refactor CSV import logic in BaseImportController

--- src/controllers/baseImportController.ts
+++ src/controllers/baseImportController.ts
@@ -28,34 +28,21 @@ class BaseImportController {
 
                 if (req.file.mimetype === 'text/csv') {
                     const dataArray: any[] = [];
-                    const headers: string[] = [];
-
-                    const csvTransform = new Transform({
-                        objectMode: true,
-                        transform(chunk, encoding, callback) {
-                            const rows = chunk.toString().split('\n');
-                            rows.forEach((row: any, index: number) => {
-                                if (index === 0) {
-                                    headers.push(...row.split(','));
-                                } else {
-                                    const columns = row.split(',');
-                                    const rowData: { [key: string]: string } = {};
-                                    columns.forEach((column: any, columnIndex:number) => {
-                                        rowData[headers[columnIndex]] = column.trim();
-                                    });
-                                    dataArray.push(rowData);
-                                }
-                            });
-                            callback();
-                        }
+                    const csvTransform = csvParser({
+                        separator: ',',
+                        mapHeaders: ({ header }) => header.trim(),
+                        mapValues: ({ header, index, value }) => value.trim()
                     });
 
                     const csvReadableStream = new Readable();
                     csvReadableStream.push(req.file.buffer);
                     csvReadableStream.push(null);
                     csvReadableStream.pipe(csvTransform)
-                        .on('finish', () => {
-                            
+                        .on('data', (data) => {
+                            dataArray.push(data);
+                        })
+                        .on('end', () => {
+                            console.log(dataArray);
                             return res.status(200).json({ message: 'File uploaded successfully', data: dataArray });
                         });
                 } else {
