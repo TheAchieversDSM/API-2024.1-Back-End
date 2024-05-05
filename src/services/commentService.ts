f84cb0bc9159f02f61dd2675f456e1f29488d069
taissalomao <tais.salomao@fatec.sp.gov.br>
2024-05-05 11:34:36 -0300

    Fix nos repositories

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,11 +1,17 @@
-import { getRepository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Comment } from '../models';
+import { DataBaseSource } from '../config/database';
 
 class CommentService {
+    private commentRepository: Repository<Comment>;
+
+    constructor() {
+        this.commentRepository = DataBaseSource.getRepository(Comment);
+    }
+
     public async createComment(comment: Comment): Promise<Comment> {
         try {
-            const commentRepository = getRepository(Comment);
-            return await commentRepository.save(comment);
+            return await this.commentRepository.save(comment);
         } catch (error) {
             console.error('Error creating comment:', error);
             throw error;
@@ -14,8 +20,7 @@ class CommentService {
 
     public async getComments(): Promise<Comment[]> {
         try {
-            const commentRepository = getRepository(Comment);
-            return await commentRepository.find();
+            return await this.commentRepository.find();
         } catch (error) {
             console.error('Error getting comments:', error);
             throw error;
@@ -24,14 +29,12 @@ class CommentService {
 
     public async getCommentsByProductId(productId: string): Promise<Comment[]> {
         try {
-            const commentRepository = getRepository(Comment);
-            return await commentRepository.find({ where: { product: { id: Number(productId) } } });
+            return await this.commentRepository.find({ where: { product: { id: Number(productId) } } });
         } catch (error) {
             console.error('Error getting comment by product id:', error);
             throw error;
         }
     }
-
 }
 
 export default new CommentService();
