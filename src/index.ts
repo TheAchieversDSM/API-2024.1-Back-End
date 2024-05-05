c3cdbbc52198415fde9eb2b113af150d42cc4cf9
taissalomao <tais.salomao@fatec.sp.gov.br>
2024-05-05 11:36:32 -0300

    fix porta

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT_PRODUCTION || 5000;
+const PORT = process.env.PORT_PRODUCTION || 1313;
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
