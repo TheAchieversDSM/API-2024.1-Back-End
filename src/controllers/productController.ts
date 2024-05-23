f544274d95b1f2990bd7c8a2c0acae08be78d3b3
= <=>
2024-05-23 20:24:20 -0300

    [feat] get de recomendacoes

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -19,26 +19,26 @@ class ProductController {
     async getProductsByCategory(req: Request, res: Response) {
         const category = req.params.category;
         try {
-          const products = await productService.getProductsByCategory(category);
-          return res.json(products);
+            const products = await productService.getProductsByCategory(category);
+            return res.json(products);
         } catch (error) {
-          console.error("Error fetching products by category:", error);
-          return res.status(500).json({ message: "Internal server error" });
+            console.error("Error fetching products by category:", error);
+            return res.status(500).json({ message: "Internal server error" });
         }
-      }
+    }
 
-      async getProductsBySubCategory(req: Request, res: Response) {
+    async getProductsBySubCategory(req: Request, res: Response) {
         const subcategory = req.params.subcategory;
         try {
-          const products = await getRepository(Product).find({
-            where: { subcategory: subcategory },
-          });
-          return res.json(products);
+            const products = await getRepository(Product).find({
+                where: { subcategory: subcategory },
+            });
+            return res.json(products);
         } catch (error) {
-          console.error("Error fetching products by subcategory:", error);
-          return res.status(500).json({ message: "Internal server error" });
+            console.error("Error fetching products by subcategory:", error);
+            return res.status(500).json({ message: "Internal server error" });
         }
-      }
+    }
 
     public async deleteAllProducts(req: Request, res: Response): Promise<void> {
         try {
@@ -66,9 +66,9 @@ class ProductController {
 
     public async getProductAverageRating(req: Request, res: Response): Promise<void> {
         const productId = req.params.productId;
-        const startDate = req.query.startDate as string ;
+        const startDate = req.query.startDate as string;
         const endDate = req.query.endDate as string;
-        
+
         try {
             const averageRating = await productService.getProductAverageRating(productId, startDate, endDate);
             res.status(200).json(averageRating);
@@ -79,73 +79,85 @@ class ProductController {
     }
 
     public async getAverageRatingByCategory(req: Request, res: Response): Promise<void> {
-      const category = req.params.category;
-      const startDate = req.query.startDate as string || '2000-01-01';
-      const endDate = req.query.endDate as string || '3099-12-31';
-      const page = parseInt(req.query.page as string, 10) || 1;
-      const limit = parseInt(req.query.limit as string, 10) || 10;
-  
-      try {
-          const { averageResponse, totalPages } = await productService.getAverageRatingByCategory(category, startDate, endDate, page, limit);
-  
-          res.status(200).json({
-              data: averageResponse,
-              pagination: {
-                  page,
-                  limit,
-                  totalPages,
-              },
-          });
-      } catch (error) {
-          console.error('Error fetching product average rating by category:', error);
-          res.status(500).json({ message: 'Internal server error' });
-      }
-  }
-  
-  public async getCommentCountByState(req: Request, res: Response): Promise<void> {
-    const state = req.params.state;
-    const startDate = req.query.startDate as string || '2000-01-01';
-    const endDate = req.query.endDate as string || '3099-12-31';
-    
-    try {
-        const commentCount = await productService.getCommentCountByState(state, startDate, endDate);
-        res.status(200).json({ commentCount });
-    } catch (error) {
-        console.error('Error fetching comment count by state:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-public async getAverageRatingByStateAndProduct(req: Request, res: Response): Promise<void> {
-    const state = req.params.state;
-    const productId = req.params.productId;
-    const startDate = req.query.startDate as string || '2000-01-01';
-    const endDate = req.query.endDate as string || '3099-12-31';
-
-    try {
-        const averageRating = await productService.getAverageRatingByStateAndProduct(state, productId, startDate, endDate);
-        res.status(200).json(averageRating);
-    } catch (error) {
-        console.error('Error fetching average rating by state and product:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-public async getProductDemography(req: Request, res: Response): Promise<void> {
-    try {
-        const productId = req.params.productId; 
+        const category = req.params.category;
         const startDate = req.query.startDate as string || '2000-01-01';
         const endDate = req.query.endDate as string || '3099-12-31';
-        const state = req.params.state;
+        const page = parseInt(req.query.page as string, 10) || 1;
+        const limit = parseInt(req.query.limit as string, 10) || 10;
 
-        const commentsInfo = await commentService.getCommentsAgeGenderAndRatingByDate(productId, startDate, endDate, state);
-        
-        res.json(commentsInfo);
-    } catch (error) {
-        console.error('Error in getCommentsAgeGenderAndRatingByDateController:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        try {
+            const { averageResponse, totalPages } = await productService.getAverageRatingByCategory(category, startDate, endDate, page, limit);
+
+            res.status(200).json({
+                data: averageResponse,
+                pagination: {
+                    page,
+                    limit,
+                    totalPages,
+                },
+            });
+        } catch (error) {
+            console.error('Error fetching product average rating by category:', error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
     }
-}
+
+    public async getCommentCountByState(req: Request, res: Response): Promise<void> {
+        const state = req.params.state;
+        const startDate = req.query.startDate as string || '2000-01-01';
+        const endDate = req.query.endDate as string || '3099-12-31';
+
+        try {
+            const commentCount = await productService.getCommentCountByState(state, startDate, endDate);
+            res.status(200).json({ commentCount });
+        } catch (error) {
+            console.error('Error fetching comment count by state:', error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+
+    public async getAverageRatingByStateAndProduct(req: Request, res: Response): Promise<void> {
+        const state = req.params.state;
+        const productId = req.params.productId;
+        const startDate = req.query.startDate as string || '2000-01-01';
+        const endDate = req.query.endDate as string || '3099-12-31';
+
+        try {
+            const averageRating = await productService.getAverageRatingByStateAndProduct(state, productId, startDate, endDate);
+            res.status(200).json(averageRating);
+        } catch (error) {
+            console.error('Error fetching average rating by state and product:', error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+
+    public async getProductDemography(req: Request, res: Response): Promise<void> {
+        try {
+            const productId = req.params.productId;
+            const startDate = req.query.startDate as string || '2000-01-01';
+            const endDate = req.query.endDate as string || '3099-12-31';
+            const state = req.params.state;
+
+            const commentsInfo = await commentService.getCommentsAgeGenderAndRatingByDate(productId, startDate, endDate, state);
+
+            res.json(commentsInfo);
+        } catch (error) {
+            console.error('Error in getCommentsAgeGenderAndRatingByDateController:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    public async getAllRecomendation(req: Request, res: Response): Promise<void> {
+        try {
+            const productsUuid = req.params.productsUuid.split(',');
+            console.log('productsUuid:', productsUuid);
+            const recommendation = await productService.getAllRecomendationByComments(productsUuid.map((uuid) => parseInt(uuid)));
+            res.status(200).json(recommendation);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
 
 }
 
